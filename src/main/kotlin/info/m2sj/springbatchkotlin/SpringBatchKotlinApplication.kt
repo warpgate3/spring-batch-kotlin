686fb9d396278fbb8e7a8e9f7c7ee84a6545f489
warpgate3 <warpgate@naver.com>
2021-08-27 21:38:08 +0900

    init: project

---
+++ src/main/kotlin/info/m2sj/springbatchkotlin/SpringBatchKotlinApplication.kt
@@ -0,0 +1,11 @@
+package info.m2sj.springbatchkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/info/m2sj/springbatchkotlin/SpringBatchKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.m2sj.springbatchkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
