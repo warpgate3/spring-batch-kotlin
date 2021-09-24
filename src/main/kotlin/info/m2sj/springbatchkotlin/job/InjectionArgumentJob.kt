795d29504816a83eae5577f9258c4fe17081d8c2
warpgate3 <warpgate3@naver.com>
2021-09-24 17:54:10 +0900

    feat: Inject Arguments Job

---
+++ src/main/kotlin/info/m2sj/springbatchkotlin/job/InjectionArgumentJob.kt
@@ -0,0 +1,56 @@
+package info.m2sj.springbatchkotlin.job
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.ItemWriter
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
+@Configuration
+class InjectionArgumentJob(
+    val jobBuilderFactory: JobBuilderFactory,
+    val stepBuilderFactory: StepBuilderFactory
+) {
+    @Bean
+    fun inJeArgJob(): Job {
+        return jobBuilderFactory.get("inJeArgJob")
+            .start(inJeArgStep(null))
+            .incrementer(RunIdIncrementer())
+            .build()
+    }
+
+    @Bean
+    @JobScope
+    fun inJeArgStep(@Value("#{jobParameters[name]}") name: String?): Step {
+        return stepBuilderFactory.get("inJeArgStep")
+            .chunk<String, String>(10)
+            .reader(inJeArgReader(name))
+            .writer(inJeArgWriter(name))
+            .build()
+    }
+
+    @Bean(name = ["inJeArgWriter"])
+    @StepScope
+    fun inJeArgWriter(@Value("#{jobParameters[name]}") name: String?): ItemWriter<String> {
+        return ItemWriter<String> {
+            TimeUnit.SECONDS.sleep(1)
+            println("name -> ${name}")
+        }
+    }
+
+    @Bean(name = ["inJeArgReader"])
+    @StepScope
+    fun inJeArgReader(@Value("#{jobParameters[name]}") name: String?): ItemReader<String> {
+        return ItemReader<String> {
+            "[${name}]"
+        }
+    }
+}
